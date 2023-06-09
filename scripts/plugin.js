4d730496fc5bf06c33beb8713ed689c7d8ff4f04
hibi <coder101011@outlook.com>
2023-06-09 16:12:37 +0800

    commit

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -3,9 +3,15 @@ hexo.extend.filter.register("after_render:html", function (htmlContent) {
 	var css = `.φbm {
         max-height: 500px;
         object-fit: cover;
+        filter:brightness(0.95);
+        transition:all 0.2s;
+    }
+    .φbm:hover{
+        filter:unset;
+        transition:all 0.2s;
+        transform: scale(1.05);
     }
     `
-
 	// 将 CSS 样式代码插入到 HTML 中
 	return htmlContent.replace(/<\/head>/i, "<style>" + css + "</style></head>")
 })
